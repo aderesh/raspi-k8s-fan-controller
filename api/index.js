5fcfe20fec6381c6d47784c02c11c920f8273cce
andrii <deresh.andrew@hotmail.com>
2023-03-10 20:26:45 -0500

    removed unused packages

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 var express = require("express");
-const raspi = require('raspi');
-const pwm = require('raspi-soft-pwm');
+const Gpio = require('pigpio').Gpio;
+
 
 var app = express();
-
 let port = process.env.NODE_PORT || 36637;
 let interval_ms = process.env.INTERVAL_SEC || 1000;
 let expiry_ms = process.env.EXPIRY_SEC || 5 * 1000;
 let low_temp_C = process.env.LOW_TEMP_C || 60;
 let high_temp_C = process.env.HIGH_TEMP_C || 78;
 let default_temp_C = process.env.DEFAULT_TEMP_C || high_temp_C;
-let gpio = process.env.GPIO || "GPIO18";
+let gpio = parseInt(process.env.GPIO) || 18;
+
+const led = new Gpio(gpio, {mode: Gpio.OUTPUT});
 
 const map = {
 }
@@ -39,8 +40,6 @@ app.get("/node/:node/temp/:temp", function (req, res) {
   res.status(200).send(``);
 });
 
-let led;
-
 setInterval(() => {
   const now = new Date();
   for (let n in map) {
@@ -67,21 +66,21 @@ setInterval(() => {
 
   max = max / 1000; // mC -> C
 
-  let pwm = ((Math.max(low_temp_C, max) - low_temp_C)) / (high_temp_C - low_temp_C);
+  // let pwm = ((Math.max(low_temp_C, max) - low_temp_C)) / (high_temp_C - low_temp_C);
 
-  pwm = Math.max(Math.min(1, pwm), 0);
+  // pwm = Math.max(Math.min(1, pwm), 0);
 
-  led.write(pwm);
+  let pwm = ((Math.max(low_temp_C, max) - low_temp_C)) * 255 / (high_temp_C - low_temp_C);
+
+  pwm = Math.max(Math.min(255, pwm), 0);
+  pwm = Math.ceil(pwm);
 
   console.log(`Fan pwm: ${pwm}. Max: ${maxNode}(${max}C). Records: ${JSON.stringify(map)}`);
 
+  led.pwmWrite(pwm);
+
 }, interval_ms);
 
-raspi.init(() => {
-  console.log("Raspi initialized for port: " + gpio);
-  led = new pwm.SoftPWM(gpio);
-
-  var server = app.listen(port, function () {
-    console.log("app running on port.", server.address().port);
-  });
+var server = app.listen(port, function () {
+  console.log("app running on port.", server.address().port);
 });
